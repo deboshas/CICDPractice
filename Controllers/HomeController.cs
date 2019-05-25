b6ae4b8f60af5ccc6555641a878b0b713dcdd10f
deboshas <debocognizant@gmail.com>
2019-05-25 19:04:54 +0530

    master brach change for repo1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace Videowebapp.Controllers
 
             //third changes to repo1
             //fourth  changes to repo1
+            //cnages made to master branch for repo 1
         public HomeController(TelemetryClient telClient)
         {
               this. aiClient = telClient;
